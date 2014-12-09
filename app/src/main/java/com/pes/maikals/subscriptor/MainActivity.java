f1c2b4c4c22ec1a43bb2e3b43b0829b79651fa03
maikals <miquicm@gmail.com>
2014-12-09 18:23:49 +0100

    App de subscriptor creant QR

---
+++ app/src/main/java/com/pes/maikals/subscriptor/MainActivity.java
@@ -0,0 +1,72 @@
+package com.pes.maikals.subscriptor;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static Bitmap toBitmap(BitMatrix matrix){
+        int height = matrix.getHeight();
+        int width = matrix.getWidth();
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                bmp.setPixel(x, y, matrix.get(x,y) ? Color.BLACK : Color.WHITE);
+            }
+        }
+        return bmp;
+    }
+
+    public void crearQR(View view) throws WriterException {
+        BitMatrix bm;
+        EditText qrText = (EditText) findViewById(R.id.editText);
+        QRCodeWriter qrWriter = new QRCodeWriter();
+        bm = qrWriter.encode(qrText.getText().toString(), BarcodeFormat.QR_CODE, 150, 150);
+
+        if(bm != null) {
+            ImageView image= (ImageView) findViewById(R.id.QRview);
+            image.setImageBitmap(toBitmap(bm));
+        }
+    }
+}
